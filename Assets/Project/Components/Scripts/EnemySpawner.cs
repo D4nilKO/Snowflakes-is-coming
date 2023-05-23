b05575eba4f6230bfeb1a9060f0e696d35aaa954
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2023-05-23 23:09:34 +0300

    Add Enemy Spawner

---
+++ Assets/Project/Components/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NTC.Global.Pool;
+using UnityEngine;
+using VavilichevGD.Utils.Timing;
+
+namespace Project.Components.Scripts
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private TimerType _type;
+        [SerializeField] private float timerSeconds;
+
+        private SyncedTimer _timer;
+
+        private void Awake()
+        {
+            _timer = new SyncedTimer(_type, timerSeconds);
+            _timer.TimerFinished += OnTimerFinished;
+            //_timer.TimerValueChanged += OnTimerValueChanged();
+
+        }
+
+        private void OnTimerFinished()
+        {
+            //NightPool.Spawn();
+        }
+
+        private void OnTimerValueChanged()
+        {
+            // Обновить число на таймере
+        }
+        
+        
+    }
+}
