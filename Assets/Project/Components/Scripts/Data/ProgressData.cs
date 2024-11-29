a500ded60d3f1b4b7929b89ab0c6baaa6d89c1e6
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2024-11-29 14:58:02 +0300

    Test

--- Assets/Project/Components/Scripts/Data/ProgressData.cs
+++ Assets/Project/Components/Scripts/Data/ProgressData.cs
@@ -1,64 +1,77 @@
 ﻿using Project.GameState;
 using UnityEngine;
+using YG;
 
 namespace Project.Data
 {
     public class ProgressData : MonoBehaviour
     {
-        [SerializeField] private GameOutcome _gameOutcome;
+        [SerializeField]
+        private GameOutcome _gameOutcome;
 
         private int _maxLevelsCount;
 
         public int CurrentLevelNumber { get; private set; } = 1;
         public int UnlockedLevelNumber { get; private set; } = 1;
 
+        #region Public methods
+
         public void IncreaseCurrentLevel()
         {
             if (CurrentLevelNumber != _maxLevelsCount && CurrentLevelNumber <= UnlockedLevelNumber)
-            {
                 CurrentLevelNumber++;
-            }
         }
 
         public void SetMaxLevelsCount(int maxLevelsCount)
         {
             if (maxLevelsCount <= 0)
             {
-                Debug.LogError("Максимальное количество уровней должно быть больше нуля");
+                Debug.LogError("Максимальное количество уровней должно быть больше нуля");
                 return;
             }
 
             _maxLevelsCount = maxLevelsCount;
         }
 
-        private void Awake()
+        #endregion
+
+        private void Start()
         {
-            SubscribeEvents();
+            if (YandexGame.SDKEnabled)
+                GetLoad();
         }
 
-        private void OnDestroy()
-        {
-            UnsubscribeEvents();
-        }
+        private void OnEnable() => SubscribeEvents();
+        private void OnDisable() => UnsubscribeEvents();
 
         private void SubscribeEvents()
         {
             if (_gameOutcome != null)
-            {
                 _gameOutcome.GameIsWon += IncreaseUnlockedLevelNumber;
-            }
             else
-            {
                 Debug.LogError("_gameOutcome is null. Cannot subscribe to events.");
-            }
+
+            YandexGame.GetDataEvent += GetLoad;
         }
 
         private void UnsubscribeEvents()
         {
             if (_gameOutcome != null)
-            {
                 _gameOutcome.GameIsWon -= IncreaseUnlockedLevelNumber;
-            }
+
+            YandexGame.GetDataEvent -= GetLoad;
+        }
+
+        private void GetLoad()
+        {
+            UnlockedLevelNumber = YandexGame.savesData.unlockedLevelNumber;
+        }
+
+        private void SaveData()
+        {
+            YandexGame.savesData.unlockedLevelNumber = UnlockedLevelNumber;
+            YandexGame.SaveProgress();
+            Debug.Log("Progress data saved");
         }
 
         private void IncreaseUnlockedLevelNumber()
@@ -70,6 +83,7 @@ namespace Project.Data
                 return;
 
             UnlockedLevelNumber = CurrentLevelNumber + 1;
+            SaveData();
         }
     }
 }
---
+++ Assets/Project/Components/Scripts/Services/SaveService.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Project.Services
+{
+    public class SaveService : MonoBehaviour
+    {
+        
+    }
+}
